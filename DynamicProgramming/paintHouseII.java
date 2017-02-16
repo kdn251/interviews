2baa6a8e5e5d0130146c2b602956a4b5adac3052
Kevin Naughton Jr <kdn251@nyu.edu>
2017-02-16 14:17:57 -0500

    added Queue

--- DynamicProgramming/paintHouseII.java
+++ DynamicProgramming/paintHouseII.java
@@ -1,4 +1,4 @@
-// There are a row of n houses, each house can be painted with one of the k colors. The cost of painting each house with a certain color is different. You have to paint all the houses such that no two adjacent houses have the same color.
+    // There are a row of n houses, each house can be painted with one of the k colors. The cost of painting each house with a certain color is different. You have to paint all the houses such that no two adjacent houses have the same color.
 
 // The cost of painting each house with a certain color is represented by a n x k cost matrix. For example, costs[0][0] is the cost of painting house 0 with color 0; costs[1][2] is the cost of painting house 1 with color 2, and so on... Find the minimum cost to paint all houses.
 
