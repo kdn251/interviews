6ba271b5dae61ea1d639d2defbeb2117a81b8821
Fatos Morina <fatosi.morina@gmail.com>
2017-04-08 20:26:06 +0200

    remove unused method at JollyJumper

--- UVa/JollyJumper.java
+++ UVa/JollyJumper.java
@@ -42,14 +42,4 @@ public class JollyJumper {
 		}
 	}
 
-	public static int findMaximalElement(int[] numbers) {
-		int max = numbers[0];
-		for (int i = 1; i < numbers.length; i++) {
-			if (max < numbers[i]) {
-				max = numbers[i];
-			}
-		}
-		return max;
-	}
-
 }
