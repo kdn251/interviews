754ff914879a28aea79535d554f28d193f4de051
Fatos Morina <fatosi.morina@gmail.com>
2017-04-12 06:44:08 +0200

    Use java doc commenting style at LastNonZeroDigit

--- UVa/LastNonZeroDigit.java
+++ UVa/LastNonZeroDigit.java
@@ -1,28 +1,28 @@
 
 /**
  * In this problem you will be given two decimal integer number N, M. You will have to find the last
-non-zero digit of the P
-N
-M . This means no of permutations of N things taking M at a time.
-Input
-The input file contains several lines of input. Each line of the input file contains two integers N
-(0 ≤ N ≤ 20000000), M (0 ≤ M ≤ N). Input is terminated by end-of-file.
-Output
-For each line of the input file you should output a single digit, which is the last non-zero digit of P
-N
-M .
-For example, if P
-N
-M is 720 then the last non-zero digit is 2. So in this case your output should be 2.
-Sample Input
-10 10
-10 5
-25 6
-Sample Output
-8
-4
-2
- */
+*non-zero digit of the P
+*N
+*M . This means no of permutations of N things taking M at a time.
+*Input
+*The input file contains several lines of input. Each line of the input file contains two integers N
+*(0 ≤ N ≤ 20000000), M (0 ≤ M ≤ N). Input is terminated by end-of-file.
+*Output
+*For each line of the input file you should output a single digit, which is the last non-zero digit of P
+*N
+*M .
+*For example, if P
+*N
+*M is 720 then the last non-zero digit is 2. So in this case your output should be 2.
+*Sample Input
+*10 10
+*10 5
+*25 6
+*Sample Output
+*8
+*4
+*2
+*/
 import static java.lang.System.exit;
 
 import java.util.Scanner;
