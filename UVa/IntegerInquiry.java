f2e4a588f89cafd7f2e68dc9cf11f6bdc9914034
Fatos Morina <fatosi.morina@gmail.com>
2017-05-05 11:24:05 +0200

    Refactor IntegerInquiry

--- UVa/IntegerInquiry.java
+++ UVa/IntegerInquiry.java
@@ -1,18 +1,9 @@
-import static java.lang.Integer.parseInt;
-import static java.lang.System.exit;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
 import java.math.BigInteger;
 import java.util.Scanner;
-import java.util.StringTokenizer;
 
 public class IntegerInquiry {
 
-	static void solve() throws Exception {
+	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		BigInteger sum = BigInteger.ZERO;
 		while (true) {
@@ -25,32 +16,4 @@ public class IntegerInquiry {
 		System.out.println(sum);
 		input.close();
 	}
-
-	static int nextInt() throws IOException {
-		return parseInt(next());
-	}
-
-	static String next() throws IOException {
-		while (tok == null || !tok.hasMoreTokens()) {
-			tok = new StringTokenizer(in.readLine());
-		}
-		return tok.nextToken();
-	}
-
-	public static void main(String[] args) {
-		try {
-			in = new BufferedReader(new InputStreamReader(System.in));
-			out = new PrintWriter(new OutputStreamWriter(System.out));
-			solve();
-			in.close();
-			out.close();
-		} catch (Throwable e) {
-			e.printStackTrace();
-			exit(0);
-		}
-	}
-
-	static BufferedReader in;
-	static PrintWriter out;
-	static StringTokenizer tok;
 }
