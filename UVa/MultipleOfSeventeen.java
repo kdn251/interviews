058a916add14133681b3427d407fb7cc2e022e37
Fatos Morina <fatosi.morina@gmail.com>
2017-05-05 11:35:52 +0200

    Add problem description for MultipleOfSeventeen

--- UVa/MultipleOfSeventeen.java
+++ UVa/MultipleOfSeventeen.java
@@ -1,3 +1,27 @@
+/**
+ * Theorem: If you drop the last digit d of an integer n (n ≥ 10), subtract 5d from the
+ * remaining integer, then the difference is a multiple of 17 if and only if n is a multiple of 17.
+ * For example, 34 is a multiple of 17, because 3-20=-17 is a multiple of 17; 201 is not a multiple of
+ * 17, because 20-5=15 is not a multiple of 17.
+ * Given a positive integer n, your task is to determine whether it is a multiple of 17.
+ * Input
+ * There will be at most 10 test cases, each containing a single line with an integer n (1 ≤ n ≤ 10100).
+ * The input terminates with n = 0, which should not be processed.
+ * Output
+ * For each case, print 1 if the corresponding integer is a multiple of 17, print 0 otherwise.
+ * Sample Input
+ * 34
+ * 201
+ * 2098765413
+ * 1717171717171717171717171717171717171717171717171718
+ * 0
+ * Sample Output
+ * 1
+ * 0
+ * 1
+ * 0
+ */
+
 //https://uva.onlinejudge.org/index.php?option=onlinejudge&Itemid=99999999&page=show_problem&category=&problem=3001
 
 import static java.lang.Integer.parseInt;
