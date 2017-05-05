ede55ffc9d64221897f8ac136517d05b57dfa864
Fatos Morina <fatosi.morina@gmail.com>
2017-05-05 09:21:15 +0200

    Add problem description for SolveEquation

--- UVa/SolveEquation.java
+++ UVa/SolveEquation.java
@@ -1,4 +1,28 @@
-
+/**
+ * Let us look at a boring mathematics problem. :-)
+ * We have three different integers, x, y and z, which satisfy the following three relations:
+ * • x + y + z = A
+ * • xyz = B
+ * • x
+ * 2 + y
+ * 2 + z
+ * 2 = C
+ * You are asked to write a program that solves for x, y and z for given values of A, B and C.
+ * Input
+ * The first line of the input file gives the number of test cases N (N < 20). Each of the following N lines
+ * gives the values of A, B and C (1 ≤ A, B, C ≤ 10000).
+ * Output
+ * For each test case, output the corresponding values of x, y and z. If there are many possible answers,
+ * choose the one with the least value of x. If there is a tie, output the one with the least value of y.
+ * If there is no solution, output the line ‘No solution.’ instead.
+ * Sample Input
+ * 2
+ * 1 2 3
+ * 6 6 14
+ * Sample Output
+ * No solution.
+ * 1 2 3
+ */
 //https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=2612
 
 import static java.lang.Integer.parseInt;
