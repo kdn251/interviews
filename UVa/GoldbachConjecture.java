6e8569fa49647c8f89bc2a4c9e2c8dbf5422011f
Fatos Morina <fatosi.morina@gmail.com>
2017-05-05 09:05:37 +0200

    Refactor GoldbachConjecture

--- UVa/GoldbachConjecture.java
+++ UVa/GoldbachConjecture.java
@@ -1,20 +1,14 @@
 
 //https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=484
 
-import static java.lang.Integer.parseInt;
-import static java.lang.System.exit;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class GoldbachConjecture {
 
-	static void solve() throws Exception {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
 		boolean[] isPrime = generatePrimeNumbers(1000000);
-		int number = nextInt();
+		int number = input.nextInt();
 		while (number != 0) {
 			boolean found = false;
 			for (int i = 3; i < number && !found; i++) {
@@ -31,7 +25,7 @@ public class GoldbachConjecture {
 			if (!found) {
 				System.out.println("Goldbach's conjecture is wrong.");
 			}
-			number = nextInt();
+			number = input.nextInt();
 		}
 	}
 
@@ -50,32 +44,4 @@ public class GoldbachConjecture {
 		}
 		return isPrime;
 	}
-
-	static int nextInt() throws IOException {
-		return parseInt(next());
-	}
-
-	static String next() throws IOException {
-		while (tok == null || !tok.hasMoreTokens()) {
-			tok = new StringTokenizer(in.readLine());
-		}
-		return tok.nextToken();
-	}
-
-	public static void main(String[] args) {
-		try {
-			in = new BufferedReader(new InputStreamReader(System.in));
-			out = new PrintWriter(new OutputStreamWriter(System.out));
-			solve();
-			in.close();
-			out.close();
-		} catch (Throwable e) {
-			e.printStackTrace();
-			exit(0);
-		}
-	}
-
-	static BufferedReader in;
-	static PrintWriter out;
-	static StringTokenizer tok;
 }
