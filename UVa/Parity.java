7ea6e0de742cea4ff9ec05bfff57ebcadf1a81cb
Fatos Morina <fatosi.morina@gmail.com>
2017-05-05 09:27:47 +0200

    Add problem description for Parity

--- UVa/Parity.java
+++ UVa/Parity.java
@@ -1,3 +1,27 @@
+/**
+ * We define the parity of an integer n as the sum of the bits in binary representation computed modulo
+ * two. As an example, the number 21 = 101012 has three 1s in its binary representation so it has parity
+ * 3(mod2), or 1.
+ * In this problem you have to calculate the parity of an integer 1 ≤ I ≤ 2147483647.
+ * Input
+ * Each line of the input has an integer I and the end of the input is indicated by a line where I = 0 that
+ * should not be processed.
+ * Output
+ * For each integer I in the inputt you should print a line ‘The parity of B is P (mod 2).’, where B
+ * is the binary representation of I.
+ * Sample Input
+ * 1
+ * 2
+ * 10
+ * 21
+ * 0
+ * Sample Output
+ * The parity of 1 is 1 (mod 2).
+ * The parity of 10 is 1 (mod 2).
+ * The parity of 1010 is 2 (mod 2).
+ * The parity of 10101 is 3 (mod 2).
+ */
+
 import static java.lang.Integer.parseInt;
 import static java.lang.System.exit;
 
