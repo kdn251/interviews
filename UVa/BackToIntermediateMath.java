9172d9036b9c716f05251dd0c3c98daf63ae38fa
Fatos Morina <fatosi.morina@gmail.com>
2017-05-05 09:58:50 +0200

    Add problem source URL for BackToIntermediateMath

--- UVa/BackToIntermediateMath.java
+++ UVa/BackToIntermediateMath.java
@@ -1,24 +1,16 @@
-
-import static java.lang.Integer.parseInt;
-import static java.lang.System.exit;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
 import java.text.DecimalFormat;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class BackToIntermediateMath {
 
-	static void solve() throws Exception {
-		int numberOfTestCases = nextInt();
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int numberOfTestCases = input.nextInt();
 		DecimalFormat formatter = new DecimalFormat("#0.000");
 		for (int i = 0; i < numberOfTestCases; i++) {
-			double distance = nextDouble();
-			double riverSpeed = nextDouble();
-			double boatSpeed = nextDouble();
+			double distance = input.nextDouble();
+			double riverSpeed = input.nextDouble();
+			double boatSpeed = input.nextDouble();
 			if (riverSpeed == 0 || boatSpeed == 0 || boatSpeed <= riverSpeed) {
 				System.out.println("Case " + (i + 1) + ": can't determine");
 			} else {
@@ -31,36 +23,4 @@ public class BackToIntermediateMath {
 			}
 		}
 	}
-
-	static int nextInt() throws IOException {
-		return parseInt(next());
-	}
-
-	static int nextDouble() throws IOException {
-		return parseInt(next());
-	}
-
-	static String next() throws IOException {
-		while (tok == null || !tok.hasMoreTokens()) {
-			tok = new StringTokenizer(in.readLine());
-		}
-		return tok.nextToken();
-	}
-
-	public static void main(String[] args) {
-		try {
-			in = new BufferedReader(new InputStreamReader(System.in));
-			out = new PrintWriter(new OutputStreamWriter(System.out));
-			solve();
-			in.close();
-			out.close();
-		} catch (Throwable e) {
-			e.printStackTrace();
-			exit(0);
-		}
-	}
-
-	static BufferedReader in;
-	static PrintWriter out;
-	static StringTokenizer tok;
 }
