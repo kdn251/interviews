1cce08ab9ac8c5977a3683e4b61faf6bc93e4944
Fatos Morina <fatosi.morina@gmail.com>
2017-05-05 07:43:18 +0200

    Refactor LightMoreLight

--- UVa/LightMoreLight.java
+++ UVa/LightMoreLight.java
@@ -1,62 +1,23 @@
-import static java.lang.Integer.parseInt;
-import static java.lang.Long.parseLong;
-import static java.lang.System.exit;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class LightMoreLight {
 
-	static void solve() throws Exception {
-		long number = nextLong();
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		long number = input.nextLong();
 		while (number != 0) {
 			if (isAPerfectSquare(number)) {
 				System.out.println("yes");
 			} else {
 				System.out.println("no");
 			}
-			number = nextLong();
+			number = input.nextLong();
 		}
 	}
 
-	static boolean isAPerfectSquare(long number) {
+	private static boolean isAPerfectSquare(long number) {
 		long squareRoot = (long) Math.sqrt(number);
 		return squareRoot * squareRoot == number;
 	}
 
-	static int nextInt() throws IOException {
-		return parseInt(next());
-	}
-
-	static long nextLong() throws IOException {
-		return parseLong(next());
-	}
-
-	static String next() throws IOException {
-		while (tok == null || !tok.hasMoreTokens()) {
-			tok = new StringTokenizer(in.readLine());
-		}
-		return tok.nextToken();
-	}
-
-	public static void main(String[] args) {
-		try {
-			in = new BufferedReader(new InputStreamReader(System.in));
-			out = new PrintWriter(new OutputStreamWriter(System.out));
-			solve();
-			in.close();
-			out.close();
-		} catch (Throwable e) {
-			e.printStackTrace();
-			exit(0);
-		}
-	}
-
-	static BufferedReader in;
-	static PrintWriter out;
-	static StringTokenizer tok;
 }
