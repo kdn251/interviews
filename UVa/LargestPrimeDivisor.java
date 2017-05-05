814477f9bb130fddc2e5eba8c896fa9b9aca8a77
Fatos Morina <fatosi.morina@gmail.com>
2017-05-05 09:12:40 +0200

    Refactor LargestPrimeDivisor

--- UVa/LargestPrimeDivisor.java
+++ UVa/LargestPrimeDivisor.java
@@ -1,19 +1,10 @@
-
-import static java.lang.Integer.parseInt;
-import static java.lang.Long.parseLong;
-import static java.lang.System.exit;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class LargestPrimeDivisor {
 
-	static void solve() throws Exception {
-		long number = nextLong();
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		long number = input.nextLong();
 		while (number != 0) {
 			number = (long) (Math.abs(number));
 			long largestPrimeDivisor = -1;
@@ -35,39 +26,7 @@ public class LargestPrimeDivisor {
 			} else {
 				System.out.println(largestPrimeDivisor);
 			}
-			number = nextLong();
+			number = input.nextLong();
 		}
 	}
-
-	static int nextInt() throws IOException {
-		return parseInt(next());
-	}
-
-	static long nextLong() throws IOException {
-		return parseLong(next());
-	}
-
-	static String next() throws IOException {
-		while (tok == null || !tok.hasMoreTokens()) {
-			tok = new StringTokenizer(in.readLine());
-		}
-		return tok.nextToken();
-	}
-
-	public static void main(String[] args) {
-		try {
-			in = new BufferedReader(new InputStreamReader(System.in));
-			out = new PrintWriter(new OutputStreamWriter(System.out));
-			solve();
-			in.close();
-			out.close();
-		} catch (Throwable e) {
-			e.printStackTrace();
-			exit(0);
-		}
-	}
-
-	static BufferedReader in;
-	static PrintWriter out;
-	static StringTokenizer tok;
 }
