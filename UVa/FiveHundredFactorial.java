73aa9952f0d28acd1958c17f6c6be4cc7a6e6965
Fatos Morina <fatosi.morina@gmail.com>
2017-05-05 08:54:02 +0200

    Refactor FiveHundredFactorial

--- UVa/FiveHundredFactorial.java
+++ UVa/FiveHundredFactorial.java
@@ -1,4 +1,3 @@
-
 /**
  * In these days you can more and more often happen to see programs which perform some useful calculations
  * being executed rather then trivial screen savers. Some of them check the system message queue
@@ -38,16 +37,8 @@
  */
 //https://uva.onlinejudge.org/index.php?option=onlinejudge&Itemid=99999999&page=show_problem&category=&problem=564
 
-
-import static java.lang.Integer.parseInt;
-import static java.lang.System.exit;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.math.BigInteger;
 import java.util.Scanner;
-import java.util.StringTokenizer;
 
 public class FiveHundredFactorial {
 
@@ -63,28 +54,4 @@ public class FiveHundredFactorial {
 		}
 		input.close();
 	}
-
-	static int nextInt() throws IOException {
-		return parseInt(next());
-	}
-
-	static String next() throws IOException {
-		while (tok == null || !tok.hasMoreTokens()) {
-			tok = new StringTokenizer(in.readLine());
-		}
-		return tok.nextToken();
-	}
-
-	public static void main(String[] args) {
-		try {
-			solve();
-		} catch (Throwable e) {
-			e.printStackTrace();
-			exit(0);
-		}
-	}
-
-	static BufferedReader in;
-	static PrintWriter out;
-	static StringTokenizer tok;
 }
