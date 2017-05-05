96ecda64326a30eabf932a10798ecc934b2316d6
Fatos Morina <fatosi.morina@gmail.com>
2017-05-05 09:43:01 +0200

    Refactor CoconutRevisited

--- UVa/CoconutsRevisited.java
+++ UVa/CoconutsRevisited.java
@@ -1,21 +1,14 @@
-import static java.lang.Integer.parseInt;
-import static java.lang.System.exit;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class CoconutsRevisited {
 
 	static void solve() throws Exception {
+		Scanner input = new Scanner(System.in);
 		int i, rez, j;
 		boolean isValid;
 		while (true) {
 			isValid = false;
-			int num = nextInt();
+			int num = input.nextInt();
 			if (num == -1) {
 				break;
 			}
@@ -38,32 +31,4 @@ public class CoconutsRevisited {
 		}
 
 	}
-
-	static int nextInt() throws IOException {
-		return parseInt(next());
-	}
-
-	static String next() throws IOException {
-		while (tok == null || !tok.hasMoreTokens()) {
-			tok = new StringTokenizer(in.readLine());
-		}
-		return tok.nextToken();
-	}
-
-	public static void main(String[] args) {
-		try {
-			in = new BufferedReader(new InputStreamReader(System.in));
-			out = new PrintWriter(new OutputStreamWriter(System.out));
-			solve();
-			in.close();
-			out.close();
-		} catch (Throwable e) {
-			e.printStackTrace();
-			exit(0);
-		}
-	}
-
-	static BufferedReader in;
-	static PrintWriter out;
-	static StringTokenizer tok;
 }
