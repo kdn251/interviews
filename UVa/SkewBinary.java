0e6ac66c25dac29ccda86d198d5066f6ca89e775
Fatos Morina <fatosi.morina@gmail.com>
2017-05-05 09:31:20 +0200

    Add SkewBinary

--- UVa/SkewBinary.java
+++ UVa/SkewBinary.java
@@ -1,20 +1,9 @@
-
-import static java.lang.Integer.parseInt;
-import static java.lang.Long.parseLong;
-import static java.lang.System.exit;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
 import java.math.BigInteger;
 import java.util.Scanner;
-import java.util.StringTokenizer;
 
 public class SkewBinary {
 
-	static void solve() throws Exception {
+	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		while (true) {
 			BigInteger number = input.nextBigInteger();
@@ -33,38 +22,5 @@ public class SkewBinary {
 			System.out.println(sum);
 		}
 		input.close();
-
 	}
-
-	static int nextInt() throws IOException {
-		return parseInt(next());
-	}
-
-	static long nextLong() throws IOException {
-		return parseLong(next());
-	}
-
-	static String next() throws IOException {
-		while (tok == null || !tok.hasMoreTokens()) {
-			tok = new StringTokenizer(in.readLine());
-		}
-		return tok.nextToken();
-	}
-
-	public static void main(String[] args) {
-		try {
-			in = new BufferedReader(new InputStreamReader(System.in));
-			out = new PrintWriter(new OutputStreamWriter(System.out));
-			solve();
-			in.close();
-			out.close();
-		} catch (Throwable e) {
-			e.printStackTrace();
-			exit(0);
-		}
-	}
-
-	static BufferedReader in;
-	static PrintWriter out;
-	static StringTokenizer tok;
 }
