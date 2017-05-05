f91f9cb520b78a9819c205eeadb5255df284310c
Fatos Morina <fatosi.morina@gmail.com>
2017-05-05 09:20:01 +0200

    Add problem source URL for SolveEquation

--- UVa/SolveEquation.java
+++ UVa/SolveEquation.java
@@ -1,4 +1,5 @@
 
+//https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=2612
 
 import static java.lang.Integer.parseInt;
 import static java.lang.System.exit;
