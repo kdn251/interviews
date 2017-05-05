65f10bd499ef108e6bdd32022f98e710728fb75c
Fatos Morina <fatosi.morina@gmail.com>
2017-05-05 11:30:23 +0200

    Add problem description at Modex

--- UVa/Modex.java
+++ UVa/Modex.java
@@ -1,4 +1,32 @@
-package mathematics.bigInteger;
+/**
+ * Many well-known cryptographic operations require modular exponentiation. That is, given integers x,
+ * y and n, compute x
+ * y mod n. In this question, you are tasked to program an efficient way to execute
+ * this calculation.
+ * Input
+ * The input consists of a line containing the number c of datasets, followed by c datasets, followed by a
+ * line containing the number ‘0’.
+ * Each dataset consists of a single line containing three positive integers, x, y, and n, separated by
+ * blanks. You can assume that 1 < x, n < 2
+ * 15 = 32768, and 0 < y < 2
+ * 31 = 2147483648.
+ * Output
+ * The output consists of one line for each dataset. The i-th line contains a single positive integer z such
+ * that
+ * z = x
+ * y mod n
+ * for the numbers x, y, z given in the i-th input dataset.
+ * Sample Input
+ * 2
+ * 2 3 5
+ * 2 2147483647 13
+ * 0
+ * Sample Output
+ * 3
+ * 11
+ */
+
+//https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=3671
 
 import static java.lang.Integer.parseInt;
 import static java.lang.System.exit;
