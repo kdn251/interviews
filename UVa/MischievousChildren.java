bc2e2ca2f96cad5324298cf8cbf09bae057dd80d
Fatos Morina <fatosi.morina@gmail.com>
2017-05-05 09:00:17 +0200

    Add problem description for MischeviousChildren

--- UVa/MischievousChildren.java
+++ UVa/MischievousChildren.java
@@ -1,3 +1,29 @@
+/**
+ * Adam’s parents put up a sign that says “CONGRATULATIONS”. The sign is so big that exactly one
+ * letter fits on each panel. Some of Adam’s younger cousins got bored during the reception and decided
+ * to rearrange the panels. How many unique ways can the panels be arranged (counting the original
+ * arrangement)?
+ * Input
+ * The first line of input is a single non-negative integer. It indicates the number of data sets to follow.
+ * Its value will be less than 30001.
+ * Each data set consists of a single word, in all capital letters.
+ * Each word will have at most 20 letters. There will be no spaces or other punctuation.
+ * The number of arrangements will always be able to fit into an unsigned long int. Note that 12!
+ * is the largest factorial that can fit into an unsigned long int.
+ * Output
+ * For each word, output the number of unique ways that the letters can be rearranged (counting the
+ * original arrangement). Use the format shown in Sample Output, below.
+ * Sample Input
+ * 3
+ * HAPPY
+ * WEDDING
+ * ADAM
+ * Sample Output
+ * Data set 1: 60
+ * Data set 2: 2520
+ * Data set 3: 12
+ */
+
 
 //https://uva.onlinejudge.org/index.php?option=onlinejudge&Itemid=99999999&page=show_problem&category=&problem=1279
 
