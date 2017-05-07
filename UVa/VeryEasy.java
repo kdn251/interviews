6dda96f0d4af75763e0329569fa6c2007cc7a8c0
Fatos Morina <fatosi.morina@gmail.com>
2017-05-07 07:34:25 +0200

    Add problem description at VeryEasy

--- UVa/VeryEasy.java
+++ UVa/VeryEasy.java
@@ -1,3 +1,29 @@
+/**
+ * Most of the times, the students of Computer Science & Engineering of BUET deal with bogus, tough and
+ * very complex formulae. That is why, sometimes, even for a easy problem they think very hard and make
+ * the problem much complex to solve. But, the team members of the team “BUET PESSIMISTIC”
+ * are the only exceptions. Just like the opposite manner, they treat every hard problem as easy and so
+ * cannot do well in any contest. Today, they try to solve a series but fail for treating it as hard. Let
+ * them help.
+ * Input
+ * Just try to determine the answer for the following series
+ * ∑ 
+ * N
+ * i=1
+ * iAi
+ * You are given the value of integers N and A (1 ≤ N ≤ 150, 0 ≤ A ≤ 15).
+ * Output
+ * For each line of the input, your correct program should output the integer value of the sum in separate
+ * lines for each pair of values of N and A.
+ * Sample Input
+ * 3 3
+ * 4 4
+ * Sample Output
+ * 102
+ * 1252
+ */
+
+
 import static java.lang.Integer.parseInt;
 import static java.lang.System.exit;
 
