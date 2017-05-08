ea884063f6126db0c2ef1cd19278449d66316646
Fatos Morina <fatosi.morina@gmail.com>
2017-05-08 06:39:00 +0200

    Add problem source URL at WhatBaseIsThis

--- UVa/WhatBaseIsThis.java
+++ UVa/WhatBaseIsThis.java
@@ -1,4 +1,6 @@
 
+//https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&category=&problem=279
+
 import static java.lang.Integer.parseInt;
 import static java.lang.System.exit;
 
