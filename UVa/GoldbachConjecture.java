47e3856ccde27d86436c0c1051f2e2a556bb9c59
Fatos Morina <fatosi.morina@gmail.com>
2017-05-08 07:05:32 +0200

    Make sieveOfEratosthenes as a private method

--- UVa/GoldbachConjecture.java
+++ UVa/GoldbachConjecture.java
@@ -63,7 +63,7 @@ public class GoldbachConjecture {
 		}
 	}
 
-	static boolean[] sieveOfEratosthenes(int number) {
+	private static boolean[] sieveOfEratosthenes(int number) {
 		boolean[] isPrime = new boolean[number + 1];
 		for (int i = 2; i < number + 1; i++) {
 			isPrime[i] = true;
