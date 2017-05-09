73d6e2e734bae9d7cfea51f57f243403865a7691
Fatos Morina <fatosi.morina@gmail.com>
2017-05-09 08:07:50 +0200

    Refactor WhoSaidCrisis

--- UVa/WhoSaidCrisis.java
+++ UVa/WhoSaidCrisis.java
@@ -1,55 +1,18 @@
 
-import static java.lang.Integer.parseInt;
-import static java.lang.System.exit;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
 import java.math.BigInteger;
 import java.util.Scanner;
-import java.util.StringTokenizer;
 
 public class WhoSaidCrisis {
 
-    static void solve() throws Exception {
-	Scanner input = new Scanner(System.in);
-	int numberOfTestCases = input.nextInt();
-	while (numberOfTestCases != 0) {
-	    BigInteger first = input.nextBigInteger();
-	    BigInteger second = input.nextBigInteger();
-	    System.out.println(first.subtract(second));
-	    numberOfTestCases--;
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int numberOfTestCases = input.nextInt();
+		while (numberOfTestCases != 0) {
+			BigInteger first = input.nextBigInteger();
+			BigInteger second = input.nextBigInteger();
+			System.out.println(first.subtract(second));
+			numberOfTestCases--;
+		}
+		input.close();
 	}
-	input.close();
-    }
-
-    static int nextInt() throws IOException {
-	return parseInt(next());
-    }
-
-    static String next() throws IOException {
-	while (tok == null || !tok.hasMoreTokens()) {
-	    tok = new StringTokenizer(in.readLine());
-	}
-	return tok.nextToken();
-    }
-
-    public static void main(String[] args) {
-	try {
-	    in = new BufferedReader(new InputStreamReader(System.in));
-	    out = new PrintWriter(new OutputStreamWriter(System.out));
-	    solve();
-	    in.close();
-	    out.close();
-	} catch (Throwable e) {
-	    e.printStackTrace();
-	    exit(0);
-	}
-    }
-
-    static BufferedReader in;
-    static PrintWriter out;
-    static StringTokenizer tok;
 }
