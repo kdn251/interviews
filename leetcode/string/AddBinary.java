d7228f5a8383c1bd09ee45058f59d07d95acdf3d
Diep Huy Duc <47498782+diephuyduc@users.noreply.github.com>
2019-09-19 18:59:11 +0700

    Checking input

--- leetcode/string/AddBinary.java
+++ leetcode/string/AddBinary.java
@@ -8,7 +8,19 @@
 public class AddBinary {
     public String addBinary(String a, String b) {
         StringBuilder result = new StringBuilder();
-        
+        //Checking input-format
+        //check=1 means true, check =0 means false.
+        int check =0;
+        for(int i=0; i<a.length(); i++){
+        if(Character.toString(a.charAt(i)).equals("1") || Character.toString(a.charAt(i)).equals("0") ){
+        check =1;
+        }
+        else{
+            check =0;
+            break;
+        } }
+        //if check =1, we will calculate.
+        if(check==1){
         int carry = 0;
         int i = a.length() - 1;
         int j = b.length() - 1;
@@ -30,7 +42,8 @@ public class AddBinary {
 
         if(carry != 0) {
             result.append(carry);
-        }
+        }}
+        
 
         return result.reverse().toString();
     }
