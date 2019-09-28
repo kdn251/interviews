04295e61ec87f34983e5bbf13b8d12c4adb021ac
S2kael <laiducminh1002@gmail.com>
2019-09-28 23:24:43 +0700

    Update SpiralMatrixTest.java
    update test

--- Test/SpiralMatrixTest.java
+++ Test/SpiralMatrixTest.java
@@ -134,4 +134,88 @@ public class SpiralMatrixTest {
         }
         assertArrayEquals(result.toArray(),SpiralMatrix.spiralOrder(input).toArray());
     }
+    
+    @Test
+    public void testSpiralOrder5() {
+        //Input:
+        //[
+//            [1	2	3	],
+//            [4	5	6	],
+        //]
+        int m = 2;
+        int n = 3;
+        int[][] input = new int[m][n];
+        int c = 1;
+        
+        for (int i=0;i<m;i++){
+            for (int j=0;j<n;j++){
+                input[i][j] = c;
+                c++;
+            }
+        }
+        //Output
+        int[] r = {1, 2, 3, 6, 5, 4};
+        List<Integer> result = new ArrayList<Integer>();
+        for (int i=0;i<m*n;i++){
+            result.add(r[i]);
+        }
+        assertArrayEquals(result.toArray(),SpiralMatrix.spiralOrder(input).toArray());
+    }
+    
+    @Test
+    public void testSpiralOrder6() {
+        //Input:
+        //[
+//            [1	2	],
+//            [3	4	],
+//            [5	6	],
+        //]
+        int m = 3;
+        int n = 2;
+        int[][] input = new int[m][n];
+        int c = 1;
+        
+        for (int i=0;i<m;i++){
+            for (int j=0;j<n;j++){
+                input[i][j] = c;
+                c++;
+            }
+        }
+        //Output
+        int[] r = {1, 2, 4, 6, 5, 3};
+        List<Integer> result = new ArrayList<Integer>();
+        for (int i=0;i<m*n;i++){
+            result.add(r[i]);
+        }
+        assertArrayEquals(result.toArray(),SpiralMatrix.spiralOrder(input).toArray());
+    }
+    
+    public void testSpiralOrder7() {
+        //Input:
+        //[
+//            [1	2	3	4	5	],
+//            [6	7	8	9	10	],
+//            [11	12	13	14	15	],
+//            [16	17	18	19	20	],
+//            [21	22	23	24	25	],
+        //]
+        int m = 3;
+        int n = 2;
+        int[][] input = new int[m][n];
+        int c = 1;
+        
+        for (int i=0;i<m;i++){
+            for (int j=0;j<n;j++){
+                input[i][j] = c;
+                c++;
+            }
+        }
+        //Output
+        int[] r = {1, 2, 3, 4, 5, 10, 15, 20, 25, 24, 23, 22, 21, 16, 11, 6, 7, 8, 9, 14, 19, 18, 17, 12, 13};
+        List<Integer> result = new ArrayList<Integer>();
+        for (int i=0;i<m*n;i++){
+            result.add(r[i]);
+        }
+        assertArrayEquals(result.toArray(),SpiralMatrix.spiralOrder(input).toArray());
+    }
 }
