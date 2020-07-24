3023ede632965b9d8e3bd5573fc6574fb94183f4
Tristan <rjt.mahinay@gmail.com>
2020-07-24 21:54:26 +0800

    Initialize cubeNum outside of loop

--- company/oracle/ArmstrongNumber.java
+++ company/oracle/ArmstrongNumber.java
@@ -16,11 +16,17 @@ public class ArmstrongNumber {
 	boolean isArmstrongNumber(int num) {
 		int numTemp = num;
 		int sum = 0;
+		int cubeNum = 0;
 		while (numTemp > 0) {
-			int cubeNum = numTemp % 10;
+			cubeNum = numTemp % 10;
 			sum += Math.pow(cubeNum, 3);
 			numTemp = numTemp / 10;
 		}
 		return (sum == num);
+	}
+
+	public static void main(String... args) {
+		ArmstrongNumber an = new ArmstrongNumber();
+		System.out.println(an.isArmstrongNumber(407));
 	}
 }
