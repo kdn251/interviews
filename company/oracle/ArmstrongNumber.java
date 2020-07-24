03f9eb48b0fec4d42c73d98241ad89e208640dff
Tristan <rjt.mahinay@gmail.com>
2020-07-24 21:41:24 +0800

    Armstrong Number Checking

---
+++ company/oracle/ArmstrongNumber.java
@@ -0,0 +1,26 @@
+package com.algorithms;
+
+/**
+ *
+ * @author rjtmahinay
+ *
+ */
+
+public class ArmstrongNumber {
+
+	/**
+	 *
+	 * @param num number to be checked
+	 * @return result if it is an Armstrong number
+	 */
+	boolean isArmstrongNumber(int num) {
+		int numTemp = num;
+		int sum = 0;
+		while (numTemp > 0) {
+			int cubeNum = numTemp % 10;
+			sum += Math.pow(cubeNum, 3);
+			numTemp = numTemp / 10;
+		}
+		return (sum == num);
+	}
+}
