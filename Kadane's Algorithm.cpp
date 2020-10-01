5e9f21229c9bad55e546a30bd741ac6f096dca82
Shubhank2604 <48112078+Shubhank2604@users.noreply.github.com>
2020-10-01 19:57:25 +0530

    Added Kadane's Algorithm for Interview Purpose

---
+++ Kadane's Algorithm.cpp
@@ -0,0 +1,31 @@
+// C++ program to print largest contiguous array sum 
+#include<iostream> 
+#include<climits> 
+using namespace std; 
+
+int maxSubArraySum(int a[], int size) 
+{ 
+	int max_so_far = INT_MIN, max_ending_here = 0; 
+
+	for (int i = 0; i < size; i++) 
+	{ 
+		max_ending_here = max_ending_here + a[i]; 
+		if (max_so_far < max_ending_here) 
+			max_so_far = max_ending_here; 
+
+		if (max_ending_here < 0) 
+			max_ending_here = 0; 
+	} 
+	return max_so_far; 
+} 
+
+/*Driver program to test maxSubArraySum*/
+int main() 
+{ 
+	int a[] = {-2, -3, 4, -1, -2, 1, 5, -3}; 
+	int n = sizeof(a)/sizeof(a[0]); 
+	int max_sum = maxSubArraySum(a, n); 
+	cout << "Maximum contiguous sum is " << max_sum; 
+	return 0; 
+} 
+
