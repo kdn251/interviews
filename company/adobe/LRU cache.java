10c7767305e4bc9559be51d7e225bf89545291b9
Shruthi-Babu <31033991+Shruthi-Babu@users.noreply.github.com>
2020-10-01 21:42:44 +0530

    LRU cache problem added

---
+++ company/adobe/LRU cache.java
@@ -0,0 +1,61 @@
+public class LRUCache {
+    private class Node{
+        Node prev;
+        Node next;
+        int key;
+        int value;
+
+        public Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+            this.prev = null;
+            this.next = null;
+        }
+    }
+
+    private int capacity;
+    private HashMap<Integer, Node> hs = new HashMap<Integer, Node>();
+    private Node head = new Node(-1, -1);
+    private Node tail = new Node(-1, -1);
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        tail.prev = head;
+        head.next = tail;
+    }
+
+    public int get(int key) {
+        if(!hs.containsKey(key)) {
+            return -1;
+        }
+        Node current = hs.get(key);
+        current.prev.next = current.next;
+        current.next.prev = current.prev;
+        moveToTail(current);
+        return hs.get(key).value;
+    }
+
+    public void put(int key, int value) {
+        if( get(key) != -1) {
+            hs.get(key).value = value;
+            return;
+        }
+
+        if (hs.size() == capacity) {
+            hs.remove(head.next.key);
+            head.next = head.next.next;
+            head.next.prev = head;
+        }
+
+        Node insert = new Node(key, value);
+        hs.put(key, insert);
+        moveToTail(insert);
+    }
+
+    private void moveToTail(Node current) {
+        current.prev = tail.prev;
+        tail.prev = current;
+        current.prev.next = current;
+        current.next = tail;
+    }
+}
