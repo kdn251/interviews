5e931099a308d180d1010d53f31705e99502d3ef
Fahim Fahad <olein@users.noreply.github.com>
2020-11-27 19:51:41 +0600

    Improved For Loop conditions

--- company/bloomberg/LongestPalindromicSubstring.java
+++ company/bloomberg/LongestPalindromicSubstring.java
@@ -17,10 +17,11 @@ class LongestPalindromicSubstring {
         }
         
         String longestPalindromicSubstring = "";
-        for(int i = 0; i < s.length(); i++) {
-            for(int j = i + 1; j <= s.length(); j++) {
-                if(j - i > longestPalindromicSubstring.length() && isPalindrome(s.substring(i, j))) {
-                    longestPalindromicSubstring = s.substring(i, j);
+        for(int i = s.length()-1; i >=0; i--) {
+            for(int j = 0; j < s.length()-i; j++) {
+                if(isPalindrome(s.substring(j, j+i))) {
+                    longestPalindromicSubstring = s.substring(j, j+i);
+                    return longestPalindromicSubstring;
                 }
             }
         }
@@ -29,15 +30,11 @@ class LongestPalindromicSubstring {
     }
     
     public boolean isPalindrome(String s) {
-        int i = 0;
-        int j = s.length() - 1;
-        while(i <= j) {
-            if(s.charAt(i++) != s.charAt(j--)) {
-                return false;
-            }
-        }
+      StringBuffer  sb = new StringBuffer(s);
+
+      if(s.equals(sb.reverse().toString())) return true;  
         
-        return true;
+        return false;
     }
 }
 
