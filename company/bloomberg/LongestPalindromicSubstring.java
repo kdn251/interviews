500164646d0a8e7a93631899a4703490cddcca39
Fahim Fahad <olein@users.noreply.github.com>
2020-11-27 19:33:12 +0600

    Improved for loop conditions
    Less check required.

--- company/bloomberg/LongestPalindromicSubstring.java
+++ company/bloomberg/LongestPalindromicSubstring.java
@@ -18,9 +18,10 @@ class LongestPalindromicSubstring {
         
         String longestPalindromicSubstring = "";
         for(int i = 0; i < s.length(); i++) {
-            for(int j = i + 1; j <= s.length(); j++) {
-                if(j - i > longestPalindromicSubstring.length() && isPalindrome(s.substring(i, j))) {
+            for(int j = s.length()-1; j >= 0; j--) {
+                if(isPalindrome(s.substring(i, j))) {
                     longestPalindromicSubstring = s.substring(i, j);
+                    return longestPalindromicSubstring;
                 }
             }
         }
@@ -29,15 +30,10 @@ class LongestPalindromicSubstring {
     }
     
     public boolean isPalindrome(String s) {
-        int i = 0;
-        int j = s.length() - 1;
-        while(i <= j) {
-            if(s.charAt(i++) != s.charAt(j--)) {
-                return false;
-            }
-        }
-        
-        return true;
+      StringBuffer  sb = new StringBuffer(s);
+
+      if(s.equals(sb.reverse().toString())) return true;         
+      return false;
     }
 }
 
