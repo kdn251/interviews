b5dc9568a50304fbc9f2d760f2fc71c5160c80e4
Tanya <tanya.agrawal1_cs19@gla.ac.in>
2021-03-10 14:26:50 +0530

    We should use Deque instead of Stack because it's more sensible in terms of inheritance. The fact that Stack extends Vector is really strange.Early in Java, inheritance was overused IMO - Properties being another example.
    Deque exposes a set of operations which is all about being able to fetch/add/remove items from the start or end of a collection, iterate etc - and that's it. There's deliberately no way to access an element by position, which Stack exposes because it's a subclass of Vector.
    
    Oh, and also Stack has no interface, so if you know you need Stack operations you end up committing to a specific concrete class, which isn't usually a good idea.
    
    And earlier r.peek()<temp was sorting the stack in descending order so I had changed it to r.peek()>temp now it is sorting a stack in ascending order

--- cracking-the-coding-interview/chapter-three-stacks-and-queues/SortStack.java
+++ cracking-the-coding-interview/chapter-three-stacks-and-queues/SortStack.java
@@ -2,10 +2,11 @@
  * you may use at most one additional stack to hold items, but you may not copy the
  * elements into any other data strcuture (such as an array). The stack supports the
  * folowing operations: push, pop, peek, and isEmpty */
-
-public class SortStack {
-	public static Stack<Integer> sort(Stack<Integer> s) {
-		Stack<Integer> r = new Stack<Integer>();
+import java.util.ArrayDeque;
+import java.util.Deque;
+class SortStack {
+	public static Deque<Integer> sort(Deque<Integer> s) {
+		Deque<Integer> r = new ArrayDeque<Integer>();
 		while(!s.isEmpty()) {
 			int tmp = s.pop(); //step 1
 			while(!r.isEmpty() && r.peek() > tmp) { //step 2
