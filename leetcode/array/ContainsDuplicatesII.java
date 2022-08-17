7e8cee4a18b47edefd73f5b24c234af1979633c8
Hassen K Mohammed <kedhas@gmail.com>
2022-08-17 18:51:23 -0400

    added example

--- leetcode/array/ContainsDuplicatesII.java
+++ leetcode/array/ContainsDuplicatesII.java
@@ -1,6 +1,9 @@
 //Given an array of integers and an integer k, find out whether there are two distinct indices i and 
 //j in the array such that nums[i] = nums[j] and the absolute difference between i and j is at most k.
 
+//example [2,5,-1,6,-9,5,8]. k=3, i=1, j= 5 |i-j|=4 4>3 --> false
+//example [2,3,-1,3,-9,5,8]. k=3, i=1, j= 3 |i-j|=2 2<3 --> true
+
 class ContainsDuplicatesII {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
         HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
