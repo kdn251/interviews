8ed8ab9955a4f95ce6cf753ca371703e657dc0bb
Hassen K Mohammed <kedhas@gmail.com>
2022-08-17 20:54:23 -0400

    Solved without extra space
    The suggestion was to solve it without extra space, but this example uses hash map that needs extra space.
    My solution here will not need extra space

--- leetcode/array/FindAllNumbersDisappearedInAnArray.java
+++ leetcode/array/FindAllNumbersDisappearedInAnArray.java
@@ -14,24 +14,17 @@
 
 class FindAllNumbersDisappearedInAnArray {
     public List<Integer> findDisappearedNumbers(int[] nums) {
-        List<Integer> result = new ArrayList<Integer>();
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-        for(int i = 1; i <= nums.length; i++) {
-            map.put(i, 1);
-        }
-        
-        for(int i = 0; i < nums.length; i++) {
-            if(map.containsKey(nums[i])) {
-                map.put(nums[i], -1);
+         List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < ary.length; i++) {
+            if (ary[i] < i + 1) {
+                result.add(i + 1);
+            } else if (ary[i] > i + 1 && ary[ary[i] - 1]!=ary[i]) {
+                int temp = ary[ary[i] - 1];
+                ary[ary[i] - 1] = ary[i];
+                ary[i] = temp;
+                i--;
             }
         }
-        
-        for(int i: map.keySet()) {
-            if(map.get(i) != -1) {
-                result.add(i);
-            }
-        }
-        
         return result;
     }
 }
