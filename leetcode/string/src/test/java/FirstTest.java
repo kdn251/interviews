2b774d1638254a52374ab43f14ccbc5ee574f87d
vynhfx01574@funix.edu.vn <vynhfx01574@funix.edu.vn>
2024-11-18 21:15:45 +0700

    Add test and refactor method isOneEditDistance in \leetcode\string\src\main\java

--- leetcode/string/src/test/java/FirstTest.java
+++
@@ -1,9 +0,0 @@
-import org.junit.jupiter.api.Test;
-
-public class FirstTest {
-    OneEditDistance oneEditDistance = new OneEditDistance();
-    @Test
-    public void setup(){
-
-    }
-}
---
+++ leetcode/string/src/test/java/OneEditDistanceRefactorTest.java
@@ -0,0 +1,65 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class OneEditDistanceRefactorTest {
+    OneEditDistanceRefactor oneEditDistance = new OneEditDistanceRefactor();
+
+
+    @Test
+    public void testIsOneEditDistance_whenInsertOneCharacterInFirstString() {
+        String str1 = "ABCD";
+        String str2 = "ABBD";
+
+        assertTrue(oneEditDistance.isOneEditDistance(str1, str2));
+    }
+
+    @Test
+    public void testIsOneEditDistance_whenDeleteOneCharactersInFirstString() {
+        String str1 = "ABCD";
+        String str2 = "ABD";
+
+        assertTrue(oneEditDistance.isOneEditDistance(str1, str2));
+    }
+
+    @Test
+    public void testIsOneEditDistance_whenAddOneCharactersInFirstString() {
+        String str1 = "ABDE";
+        String str2 = "ABCDE";
+
+        assertTrue(oneEditDistance.isOneEditDistance(str1, str2));
+    }
+
+    @Test
+    public void testIsOneEditDistance_whenTwoStringAreEquals() {
+        String str1 = "ABCD";
+        String str2 = "ABCD";
+
+        assertFalse(oneEditDistance.isOneEditDistance(str1, str2));
+    }
+
+    @Test
+    public void testIsOneEditDistance_whenInsertTwoCharactersInFirstString() {
+        String str1 = "ABCD";
+        String str2 = "ABEF";
+
+        assertFalse(oneEditDistance.isOneEditDistance(str1, str2));
+    }
+
+    @Test
+    public void testIsOneEditDistance_whenDeleteTwoCharactersInFirstString() {
+        String str1 = "ABCD";
+        String str2 = "AB";
+
+        assertFalse(oneEditDistance.isOneEditDistance(str1, str2));
+    }
+
+    @Test
+    public void testIsOneEditDistance_whenAddTwoCharactersInFirstString() {
+        String str1 = "ABCD";
+        String str2 = "ABCDEF";
+
+        assertFalse(oneEditDistance.isOneEditDistance(str1, str2));
+    }
+
+}
